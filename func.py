27a938a41f729decabc6f66cd114123947e783dd
kolhir <kolhir16@gmail.com>
2018-01-20 18:13:18 +0300

    3.0 commit

---
+++ func.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+import time
+from datetime import datetime,date
+schools =("s14","s30","s37")
+klases=("10А","10В", "9Б", "9А", "10А Не химики", "10А Химики")
+vlz_dict = {"s14":["10А Не химики", "10А Химики"],"s30":["10А","10В", "9Б", "9А",], "s37":["10А",]}
+start = "начало"
+end = "конец"
+
+l = "schools/"
+
+
+users={}#{
+            #"14":[{},[]],
+            #"30":[{"111":"10Б","222":"9Б","333":"10Б",},[123,124,127]], 
+            #"37":[{},[]]
+        #}
+
+for sch in list(vlz_dict.keys()):#shc - список школ
+    other = {sch:[{},[]]} 
+    users.update(other)
+    for kl in vlz_dict[sch]:     #kl - конкретный класс школы 
+        f=open(l + sch + "/" + kl)
+        ids=f.read()
+        ids=list(map(int,ids.split()))
+        ids = dict.fromkeys(ids, kl)
+        users[sch][0].update(ids)
+        f.close()
+    f=open(l + sch + "/" + sch)
+    ids=f.read()
+    ids=list(map(int,ids.split()))
+    users[sch][1] = ids
+
+def whoIsHe(user_id):
+    who = []
+    not_klass = [i for l in schools for i in users[l][1]]
+    if user_id in not_klass:
+        for sch in list(vlz_dict.keys()):
+            if user_id in users[sch][1]:
+                who = [sch]
+                return who
+    else:
+        for sch in list(vlz_dict.keys()):
+            if user_id in users[sch][0]:
+                who = [sch, users[sch][0][user_id]]
+                return who 
+    return who
+
+def addInSchool(user_id, school):
+    users[school][1].append(user_id)
+    f = open(l + school + "/" + school,'a')
+    f.write(str(user_id) + ' ')
+
+def addUser(user_id, klass, school):
+#Добавляем в файл класса
+    f = open((l+school+ "/" + klass),'a')
+    f.write(str(user_id) + ' ')
+    ids = {user_id:klass}
+    users[school][0].update(ids)
+    f.close()
+#Удаляем из файла для тех, у кого нет класса  
+    f = open((l+school+ "/" + school))
+    ids = f.read()
+    ids = list(map(int,ids.split()))
+    ids.remove(user_id)
+    f.close()  
+    f = open((l+school+ "/" + school),'w')
+    for i in ids: f.write(str(i)+' ')
+    f.close()
+    users[school][1].remove(user_id)
+
+def deleteUserFromKlass(user_id):
+    s = whoIsHe(user_id)
+    if (len(s) == 2):
+
+        klass = s[1]
+        school = s[0]
+        f= open((l+school+ "/" + klass))#TODO
+        ids=f.read()
+        ids = list(map(int,ids.split()))
+        ids.remove(user_id)
+        f.close()
+
+        f = open((l+school+ "/" + klass),'w')
+        for i in ids: f.write(str(i)+' ')
+        f.close()
+        users[school][0].pop(user_id)
+
+        f= open((l+school+ "/" + school), 'w')
+        f.write(str(user_id) + ' ')
+        f.close()
+        users[school][1].append(user_id)
+    
+def deleteUser(user_id):
+    s = whoIsHe(user_id)
+    
+    print(2)
+    if len(s) == 2:
+        klass = s[1]
+        school = s[0]
+        f= open((l+school+ "/" + klass))#TODO
+        ids=f.read()
+        ids = list(map(int,ids.split()))
+        ids.remove(user_id)
+
+        f = open((l+school+ "/" + klass),'w')
+        for i in ids: f.write(str(i)+' ')
+        f.close()
+        users[school][0].pop(user_id)
+
+    elif len(s) == 1:
+        school = s[0]
+        f = open((l+school+ "/" + school))
+        ids = f.read()
+        ids = list(map(int,ids.split()))
+        ids.remove(user_id)
+        f.close()  
+        f = open((l+school+ "/" + school),'w')
+        for i in ids: f.write(str(i)+' ')
+        f.close()
+        users[school][1].remove(user_id)
+
+def nextLesson(usList):
+
+    school = usList[0]
+    klass = usList[1]
+    if school == "s14":
+        from schools.s14.config import tt, timeles
+    elif school == "s30":
+        from schools.s30.config import tt, timeles
+    elif school == "s37":
+        from schools.s37.config import tt, timeles
+
+    timenow = time.strptime(time.strftime("%X", time.localtime()), "%X")
+    weekday_number = date.weekday(datetime.now())
+    weekdays=("Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье")
+    day = weekdays[weekday_number]
+    answer=""
+    flag = ""
+    if weekday_number != 5:
+        w = 0
+    elif weekday_number == 6:
+        answer = "А сегодня не воскресенье???"
+        return answer
+    else: w = 1
+    if not(tt[klass][day]):
+        answer = "Так хочется учиться сегодня??? Сорри, кажется у тебя сегодня выходной."
+        return answer
+    # print("tt = ", tt, "\n", "tt[klass] =", tt[klass], "\n", "tt[klass][day] =" , tt[klass][day])
+    for i in range(1,10):
+        start1b = time.strptime(timeles[w][start][str(i)], "%X")
+        start1e = time.strptime(timeles[w][end][str(i)], "%X")
+        start2b = time.strptime(timeles[w][start][str(i + 1)], "%X") 
+        start2e = time.strptime(timeles[w][end][str(i + 1)], "%X")
+        # if  (i == 1) and (timenow < start1b):
+        #     answer = ("Следующий урок: "+ str(tt[klass][day][str(i)]))
+        #     flag = str( tt[klass][day][str(i+1)])
+        #     break
+        if start1b <= timenow <=  start1e:
+            answer = ("Следующий урок: "+ str(tt[klass][day][str(i+1)]))
+            flag = str( tt[klass][day][str(i+1)])
+            break
+        elif start1e <= timenow <= start2b:
+            answer = ("Уже скоро начнется: " + str(tt[klass][day][str(i+1)]))
+            flag = str(tt[klass][day][str(i+1)])
+            break
+        elif (i == 9) and (start2b <= timenow <= start2e):
+            answer = ("Сейчас идет последний урок")
+            flag = str(tt[klass][day][str(i+1)])
+            break
+        elif (i == 9) and (timenow > start2e):
+            answer = ("Все, остановись, сегодня больше нет уроков")
+            flag = str(tt[klass][day][str(i+1)])
+            break
+        elif (i==1) and (timenow < start1b):
+            if (tt[klass][day]):
+                for k in range(1,10):
+                    if (tt[klass][day][str(k)]):
+                        answer = ("Следующий урок: "+ str(tt[klass][day][str(k)]))
+                        flag = str(tt[klass][day][str(k)])
+                        break
+            else:  
+                answer = "Сегодня нет уроков"
+                flag = "1"
+                break 
+    if flag:
+        return answer
+    else:
+        answer = "Сегодня больше нет уроков"
+        return answer
+
+def onDay(day, usList):
+    school = usList[0]
+    klass = usList[1]
+    if school == "s14":
+        from schools.s14.config import tt, timeles
+    elif school == "s30":
+        from schools.s30.config import tt, timeles
+    elif school == "s37":
+        from schools.s37.config import tt, timeles
+    answer = ""
+    flag = 0
+    win = ""
+
+    if day != "Суббота":
+        w = 0
+    else: w = 1
+
+    if not(tt[klass][day]):
+        answer = ("Ты разве учишься в этот день?!")
+        return answer
+
+    for i in range(1,11):
+        st = str(timeles[w][start][str(i)])
+        en = str(timeles[w][end][str(i)])
+        if (tt[klass][day][str(i)] == ""):
+            win = win + (str(i) + ". " + st[0:5] + " - " +en[0:5]+ " " + str(tt[klass][day][str(i)]) + " \n")
+            flag = flag + 1
+        else:
+           answer = answer + win + (str(i) + ". " + st[0:5] + " - " +en[0:5]+ " " + str(tt[klass][day][str(i)]) + " \n")
+           win = ""
+        if ((i == 10) and (flag == 10)) or not(tt[klass][day]):
+            answer = ("Вы не учитесь в этот день")
+            break   
+    return answer
+
+def untilTheEnd(usList):
+    def deltaplan(now_time, next_time):
+        answer=0
+        (now_time,next_time)=(list(now_time),list(next_time))
+        if now_time[3]>next_time[3] :next_time[3]=next_time[3]+24
+        if now_time[5]>next_time[5] :answer=answer-1
+        answer=answer+(next_time[3]-now_time[3])*60+next_time[4]-now_time[4]
+        return answer
+
+    def valMinute(delta):
+        answer = ""
+        if delta<=2:
+            answer = ("До конца урока осталось совсем чуть-чуть:)")
+        elif 11 <= delta <= 19:
+            answer = ("До конца урока осталось: " + str(delta) + " минут" )
+        elif delta % 10 == 1: 
+            answer = ("До конца урока осталось: " + str(delta) + " минутa" )
+        elif 2 <= (delta % 10) <= 4:
+            answer = ("До конца урока осталось: " + str(delta) + " минуты" )
+        else:
+            answer = ("До конца урока осталось: " + str(delta) + " минут" )
+        return answer
+
+    def minute(delta):
+        answer = ""
+        if 11 <= delta <= 19:
+            answer = (" минут" )
+        elif delta % 10 == 1: 
+            answer = (" минутa" )
+        elif 2 <= (delta % 10) <= 4:
+            answer = (" минуты" )
+        else:
+            answer = (" минут" )
+        return answer
+        
+    school = usList[0]
+    klass = usList[1]
+
+    if school == "s14":
+        from schools.s14.config import tt, timeles
+    elif school == "s30":
+        from schools.s30.config import tt, timeles
+    elif school == "s37":
+        from schools.s37.config import tt, timeles
+
+    timenow = time.strptime(time.strftime("%X", time.localtime()), "%X")
+    weekday_number = date.weekday(datetime.now())
+    weekdays=("Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье")
+    day = weekdays[weekday_number]
+    answer=""
+    flag = ""
+
+    if day != "Суббота":
+        w = 0
+    else: w = 1
+
+    if not(tt[klass][day]):
+        answer = ("Ты разве учишься сегодня?!")
+        return answer
+
+    for i in range(1,10):
+        start1b = time.strptime(timeles[w][start][str(i)], "%X")
+        start1e = time.strptime(timeles[w][end][str(i)], "%X")
+        start2b = time.strptime(timeles[w][start][str(i + 1)], "%X") 
+        start2e = time.strptime(timeles[w][end][str(i + 1)], "%X")
+        if start1b <= timenow <=  start1e:
+            if str(tt[klass][day][str(i)]):
+                delta = deltaplan(timenow, start1e)
+                answer = valMinute(delta)
+                break
+        elif start1e <= timenow <= start2b:
+            if str(tt[klass][day][str(i+1)]):
+                delta = deltaplan(timenow, start2b)
+                answer = ("Сейчас перемена!\nДо начала урока : " + str(delta) + minute(delta))
+                break
+        elif (i == 9) and (start2b <= timenow <= start2e):
+            if str(tt[klass][day][str(i)]):
+                delta = deltaplan(timenow, start1e)
+                answer = valMinute(delta)      
+                break   
+        else:
+            answer = ("Ой все, отстань")
+    return answer
---
+++ main.py
@@ -0,0 +1,133 @@
+import telebot, config, time
+import func as f
+
+token = config.token
+bot = telebot.TeleBot(token, threaded=False)
+# bot = telebot.TeleBot(token)
+weekdays=("Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье")
+
+def my_send_message(userid, string, user_markup, message):
+    l = f.whoIsHe(userid)
+    timenow = time.strftime("%X", time.localtime())
+    k = message.from_user
+    if(len(l) == 2):
+        print("Школа: ",l[0], "; Класс: ", l[1], "; Время: ", timenow, sep = "")
+    print(k.id, ";  Имя: ", k.first_name, ";  Фамилия: ", k.last_name, "; User_name: ", k.username, "\n", "Сообщение: ", string, "\n", sep = "")
+    return (bot.send_message(userid, string, reply_markup = user_markup))
+
+def my_send_message_WK(userid, string, message):
+    k = message.from_user
+    l = f.whoIsHe(userid)
+    timenow = time.strftime("%X", time.localtime())
+    k = message.from_user
+    if(len(l) == 2):
+        print("Школа: ",l[0], "Класс: ", l[1], "Время: ", timenow, sep = "")
+    print(k.id, "; Имя: ", k.first_name, "; Фамилия: ", k.last_name, "; User_name: ", k.username, "\n", "Сообщение: ", string, "\n", sep = "")
+    return (bot.send_message(userid, string))
+
+def start_murkup():
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('Следующий урок')
+    user_markup.row('Расписание на день')
+    user_markup.row('Сколько осталось до конца урока?')
+    user_markup.row('Изменить класс')
+    user_markup.row('Изменить школу')
+    return user_markup
+
+def choose_school(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    for i in f.schools:
+        user_markup.row(i[1:])
+    user_school = my_send_message(message.from_user.id, "Выбери школу", user_markup, message)
+
+def choose_klass(message):
+    l = f.whoIsHe(message.from_user.id)
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    if (len(l) == 1):
+        for i in f.vlz_dict[l[0]]:
+            user_markup.row(i)
+    user_markup.row("Изменить школу")
+    user_klass = my_send_message(message.from_user.id, "Выбери класс", user_markup, message)
+
+def start(message):
+    my_send_message(message.from_user.id, "Выбери действие",  start_murkup(), message)
+    
+def ttOnDay(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('Понедельник', 'Четверг')
+    user_markup.row('Вторник', 'Пятница')
+    user_markup.row('Среда', 'Суббота')
+    day = my_send_message(message.from_user.id, "Выбери день", user_markup, message)
+    bot.register_next_step_handler(day, onDay)
+
+def nextLesson(message):
+    my_send_message(message.from_user.id, f.nextLesson(f.whoIsHe(message.from_user.id)), start_murkup(), message)###Передать класс человека
+
+def untilTheEnd(message):
+    my_send_message(message.from_user.id, f.untilTheEnd(f.whoIsHe(message.from_user.id)), start_murkup() ,message)###Передать класс человека
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    if not(f.whoIsHe(message.from_user.id)):
+        choose_school(message)
+    else:
+        start(message)
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    l = f.whoIsHe(message.from_user.id)
+    if len(l) == 2: 
+        if message.text == "Следующий урок":
+            nextLesson(message)
+        elif message.text == "Расписание на день":
+            ttOnDay(message)
+        elif message.text == "Сколько осталось до конца урока?":
+            untilTheEnd(message)
+        elif message.text == "Изменить класс":
+            f.deleteUserFromKlass(message.from_user.id)
+            choose_klass(message)
+        elif message.text == "Изменить школу":
+            f.deleteUser(message.from_user.id)
+            choose_school(message)
+        else: start(message)
+    elif (len(l) == 1) and (message.text == "Изменить школу"):
+        f.deleteUser(message.from_user.id)
+        choose_school(message)
+    elif ("s"+message.text) in f.schools:
+        message.text = "s" + message.text
+        if len(l) == 0:
+            f.addInSchool(message.from_user.id, message.text)
+            choose_klass(message)
+        elif len(l) == 1:
+            choose_klass(message)
+        else:
+            start(message)
+
+    elif message.text in f.klases:
+        if len(l) == 0:
+            choose_school(message)
+        elif len(l) == 1:
+            f.addUser(message.from_user.id, message.text, l[0])
+        start(message)
+    elif len(l) == 1:
+        choose_klass(message)
+    else:
+        choose_school(message)
+
+def onDay(message): 
+    if message.text in weekdays:
+        my_send_message(message.from_user.id, f.onDay(message.text, f.whoIsHe(message.from_user.id)), start_murkup(), message)###Передать класс человека
+       
+    elif message.text == "Следующий урок":
+        nextLesson(message)
+    else: ttOnDay(message)
+while True:
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print("Ошибка: ",e)
+        import traceback; traceback.print_exc()  # или просто print(e) если у вас логгера нет,
+        # или import traceback; traceback.print_exc() для печати полной инфы
+        time.sleep(15)
+
